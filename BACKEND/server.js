f2dcab6f1b432d2e7d50349bae03a8d7d46bf916
Sam-Teche <ogunrindesam@gmail.com>
2025-06-22 23:01:51 +0100

    lalalal

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -7,7 +7,27 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://busyvotingsystem.netlify.app",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+app.options(
+  "*",
+  cors({
+    origin: "https://busyvotingsystem.netlify.app",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+
+
 app.use(express.json());
 
 // Load environment variables
